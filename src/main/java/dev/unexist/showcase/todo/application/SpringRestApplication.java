dbde83ac8fe4fe91ec16e8fa28fa435c75684bf4
Christoph Kappel <christoph@unexist.dev>
2021-08-09 16:08:47 +0200

    Added component scan path

--- src/main/java/dev/unexist/showcase/todo/application/SpringRestApplication.java
+++ src/main/java/dev/unexist/showcase/todo/application/SpringRestApplication.java
@@ -13,8 +13,10 @@ package dev.unexist.showcase.todo.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "dev.unexist.showcase.todo")
 public class SpringRestApplication {
     @SuppressWarnings("UncommentedMain")
     public static void main(String... args) {
