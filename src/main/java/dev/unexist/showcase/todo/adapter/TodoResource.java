88fbbfce42a4b30facb4a914b76498bb6910be99
Christoph Kappel <christoph@unexist.dev>
2021-08-10 18:46:53 +0200

    Updated resource for CQRS handling

--- src/main/java/dev/unexist/showcase/todo/adapter/TodoResource.java
+++ src/main/java/dev/unexist/showcase/todo/adapter/TodoResource.java
@@ -13,7 +13,8 @@ package dev.unexist.showcase.todo.adapter;
 
 import dev.unexist.showcase.todo.domain.todo.Todo;
 import dev.unexist.showcase.todo.domain.todo.TodoBase;
-import dev.unexist.showcase.todo.domain.todo.TodoService;
+import dev.unexist.showcase.todo.domain.todo.commands.CreateCommand;
+import dev.unexist.showcase.todo.domain.todo.queries.FindAllQuery;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -21,6 +22,9 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -34,13 +38,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class TodoResource {
 
     @Autowired
-    TodoService todoService;
+    private CommandGateway commandGateway;
+
+    @Autowired
+    private QueryGateway queryGateway;
 
     @Operation(summary = "Create new todo")
     @ApiResponses({
@@ -50,16 +57,8 @@ public class TodoResource {
     })
     @PostMapping(value = "/todo", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity create(@RequestBody TodoBase base) {
-        ResponseEntity response;
-
-        if (this.todoService.create(base)) {
-            response = ResponseEntity.ok().build();
-        } else {
-            response = new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-        }
-
-        return response;
+    public CompletableFuture<Void> create(@RequestBody TodoBase base) {
+        return this.commandGateway.send(new CreateCommand(base));
     }
 
     @Operation(summary = "Get all todos")
@@ -70,18 +69,8 @@ public class TodoResource {
             @ApiResponse(responseCode = "500", description = "Server error")
     })
     @GetMapping(value = "/todo", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Todo>> getAll() {
-        List<Todo> todoList = this.todoService.getAll();
-
-        ResponseEntity response;
-
-        if (todoList.isEmpty()) {
-            response = new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
-            response = ResponseEntity.ok(todoList);
-        }
-
-        return response;
+    public CompletableFuture<List<Todo>> getAll() {
+        return this.queryGateway.query(new FindAllQuery(), ResponseTypes.multipleInstancesOf(Todo.class));
     }
 
     @Operation(summary = "Get todo by id")
@@ -92,19 +81,9 @@ public class TodoResource {
             @ApiResponse(responseCode = "500", description = "Server error")
     })
     @GetMapping(value = "/todo/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Todo> findById(@Parameter(description = "Todo id")
+    public ResponseEntity findById(@Parameter(description = "Todo id")
                                              @RequestParam("id") int id) {
-        Optional<Todo> result = this.todoService.findById(id);
-
-        ResponseEntity response;
-
-        if (result.isPresent()) {
-            response = ResponseEntity.ok(result.get());
-        } else {
-            response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        return response;
+        return ResponseEntity.status(HttpStatus.GONE).build();
     }
 
     @Operation(summary = "Update todo by id")
@@ -117,29 +96,13 @@ public class TodoResource {
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity update(@Parameter(description = "Todo id")
                                      @RequestParam("id") int id, @RequestBody TodoBase base) {
-        ResponseEntity response;
-
-        if (this.todoService.update(id, base)) {
-            response = new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
-            response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        return response;
+        return ResponseEntity.status(HttpStatus.GONE).build();
     }
 
     @Operation(summary = "Delete todo by id")
     @DeleteMapping(value = "/todo/{id}", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity delete(@Parameter(description = "Todo id") @RequestParam("id") int id) {
-        ResponseEntity response;
-
-        if (this.todoService.delete(id)) {
-            response = new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
-            response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        return response;
+        return ResponseEntity.status(HttpStatus.GONE).build();
     }
 }
